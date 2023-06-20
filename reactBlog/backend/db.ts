ceae3cb8869de5833adf3a1e990c2effb0b1e009
Frontend JavaScript <frontendjavascript@C3-28.local>
2023-06-20 21:04:00 +0300

    reactBlog

---
+++ reactBlog/backend/db.ts
@@ -0,0 +1,12 @@
+// acest fisier conecteaza baza de date la .env
+
+import mysql from "mysql2";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+export const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PWD,
+  database: process.env.DB_NAME
+});
---
+++ reactBlog/backend/server.ts
@@ -0,0 +1,35 @@
+
+//
+
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import path from 'path';
+import  session, {Session} from "express-session";
+import { userRouter } from './routes/userRouter'; //routa definita
+import { postRouter } from './routes/postRouter';// ruta definita
+import { adminRouter } from './routes/adminRouter';// ruta definita
+dotenv.config();
+
+const app: Express = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+app.use(express.static(path.join(__dirname, 'static')));
+
+const port = process.env.PORT;
+app.use(cors());
+app.use("/users", userRouter);
+app.use("/posts", postRouter);
+app.use("/admin", adminRouter);
+app.get('/', (req: Request, res: Response) => {
+  //res.send('Express + TypeScript Server!!!!');
+  res.sendFile(path.join(__dirname+'/acasa.html'));
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at https://localhost:${port}`);
+});
